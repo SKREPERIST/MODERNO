96e71b00e3a32b4b213521cb0fc548a4df65a4c3
SKREPERIST <skreperist666@gmail.com>
2019-09-25 23:47:50 +0300

    added button-list

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
 $(function () {
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
 
-  var mixer = mixitup('.products__inner-box');
 
   $('.product-slider__inner').slick({
     dots: true,
@@ -11,10 +15,29 @@ $(function () {
   });
 
 
-  $(".rate-star").rateYo({
-    rating: 5,
-    starWidth: "12px",
-    readOnly: true
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    // grid: true,
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
   });
+
+
+  $('.icon-th-list').on('click', function () {
+    $('.product-page__items .products__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+
+  $('.icon-th-large').on('click', function () {
+    $('.product-page__items .products__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 })
